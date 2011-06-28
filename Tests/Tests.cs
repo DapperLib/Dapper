e1d30a43e9fec06d42e013a8a2c26c5aeb712174
Sam Saffron <sam@stackoverflow.com>
2011-06-28 12:00:03 +1000

    test for multi command with a strong type

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -226,6 +226,25 @@ namespace SqlMapper
             sum.IsEqualTo(10);
         }
 
+        class Student
+        {
+            public string Name {get; set;}
+            public int Age { get; set; }
+        }
+
+        public void TestExecuteMultipleCommandStrongType()
+        {
+            connection.Execute("create table #t(Name nvarchar(max), Age int)");
+            int tally = connection.Execute(@"insert #t (Name,Age) values(@Name, @Age)", new List<Student> 
+            {
+                new Student{Age = 1, Name = "sam"},
+                new Student{Age = 2, Name = "bob"}
+            });
+            int sum = connection.Query<int>("select sum(Age) from #t drop table #t").First();
+            tally.IsEqualTo(2);
+            sum.IsEqualTo(3);
+        }
+
         public void TestExecuteMultipleCommandObjectArray()
         {
             connection.Execute("create table #t(i int)");
