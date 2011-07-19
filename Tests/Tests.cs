0de43b95751340772a90edfdf98c74b9190ba55c
Sam Saffron <sam@stackoverflow.com>
2011-07-19 15:44:52 +1000

    fix for crazy multi mapping ... backwards compatible - allow for the Split to be in the first type.

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1036,5 +1036,30 @@ Order by p.Id";
 
             p.Get<int?>("@b").IsNull();
         }
+
+        class Foo1 
+        {
+            public int Id;
+            public int BarId { get; set; }
+        }
+
+        class Bar1
+        {
+            public int BarId;
+            public string Name { get; set; }
+        }
+
+        public void TestMultiMapperIsNotConfusedWithUnorderedCols()
+        {
+
+            var result = connection.Query<Foo1,Bar1, Tuple<Foo1,Bar1>>("select 1 as Id, 2 as BarId, 3 as BarId, 'a' as Name", (f,b) => Tuple.Create(f,b), splitOn: "BarId").First();
+
+            result.Item1.Id.IsEqualTo(1);
+            result.Item1.BarId.IsEqualTo(2);
+            result.Item2.BarId.IsEqualTo(3);
+            result.Item2.Name.IsEqualTo("a");
+            
+        }
+    
     }
 }
