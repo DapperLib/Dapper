3e0f3d215fc27911bb5a570fa39e858d3aa8986d
mgravell <none@none>
2011-07-22 12:36:28 +0100

    add automatic collection

--- Dapper/SqlMapper.cs
+++ Dapper/SqlMapper.cs
@@ -135,8 +135,34 @@ namespace Dapper
         static readonly System.Collections.Concurrent.ConcurrentDictionary<Identity, CacheInfo> _queryCache = new System.Collections.Concurrent.ConcurrentDictionary<Identity, CacheInfo>();
         private static void SetQueryCache(Identity key, CacheInfo value)
         {
+            if(Interlocked.Increment(ref collect)==COLLECT_PER_ITEMS)
+            {
+                CollectCacheGarbage();
+            }
             _queryCache[key] = value;
         }
+
+        private static void CollectCacheGarbage()
+        {
+            try
+            {
+                foreach (var pair in _queryCache)
+                {
+                    if (pair.Value.GetHitCount() <= COLLECT_HIT_COUNT_MIN)
+                    {
+                        CacheInfo cache;
+                        _queryCache.TryRemove(pair.Key, out cache);
+                    }
+                }
+            }
+            finally
+            {
+                Interlocked.Exchange(ref collect, 0);
+            }
+        }
+
+        private const int COLLECT_PER_ITEMS = 1000, COLLECT_HIT_COUNT_MIN = 0;
+        private static int collect;
         private static bool TryGetQueryCache(Identity key, out CacheInfo value)
         {
             if(_queryCache.TryGetValue(key, out value))
