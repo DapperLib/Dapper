0afbdda5288bf5ba5a14d548cf7e457a986fff44
mgravell <none@none>
2011-07-25 07:28:07 +0100

    add .NET 3.5 test rig

---
+++ DapperTests NET35/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Data.SqlClient;
+
+namespace DapperTests_NET35
+{
+    class Program
+    {
+        static void Main()
+        {
+            RunTests();
+            Console.WriteLine("(end of tests; press any key)");
+
+            Console.ReadKey();
+        }
+        public static readonly string connectionString = "Data Source=.;Initial Catalog=tempdb;Integrated Security=True";
+
+        public static SqlConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+        private static void RunTests()
+        {
+            var tester = new Tests();
+            foreach (var method in typeof(Tests).GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                Console.Write("Running " + method.Name);
+                method.Invoke(tester, null);
+                Console.WriteLine(" - OK!");
+            }
+        }
+    }
+}
