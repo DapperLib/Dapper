b55cc9a4a89648af9954967cd2236ab9f61779ef
mgravell <none@none>
2011-10-26 22:14:30 +0100

    test for SO7908830

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -342,6 +342,26 @@ namespace SqlMapper
             gotException.IsTrue();
         }
 
+        public void TestNakedBigInt()
+        {
+            long foo = 12345;
+            var result = connection.Query<long>("select @foo", new {foo}).Single();
+            foo.IsEqualTo(result);
+        }
+
+        public void TestBigIntMember()
+        {
+            long foo = 12345;
+            var result = connection.Query<WithBigInt>(@"
+declare @bar table(Value bigint)
+insert @bar values (@foo)
+select * from @bar", new {foo}).Single();
+            result.Value.IsEqualTo(foo);
+        }
+        class WithBigInt
+        {
+            public long Value { get; set; }
+        }
 
         class User
         {
