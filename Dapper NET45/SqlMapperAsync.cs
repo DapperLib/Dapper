3fccaa6268f190fc0383b163e7cfca172653a045
bkaid <brandonkaid@gmail.com>
2012-09-09 16:48:30 -0700

    Added .NET 4.5 style QueryAsync method to support asynchronous database calls. Requires SQL Server because ExecuteReaderAsync is defined on SqlCommand and not IDbCommand.

---
+++ Dapper NET45/SqlMapperAsync.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper
+{
+    public static partial class SqlMapper
+    {
+        /// <summary>
+        /// Execute a query asynchronously using .NET 4.5 Task.  Currently only SqlCommand supports ExecuteReaderAsync so SQL Server is required.
+        /// </summary>
+        public static async Task<IEnumerable<T>> QueryAsync<T>(this IDbConnection cnn, string sql, dynamic param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            var identity = new Identity(sql, commandType, cnn, typeof(T), param == null ? null : param.GetType(), null);
+            var info = GetCacheInfo(identity);
+
+            var cmd = SetupCommand(cnn, transaction, sql, info.ParamReader, param, commandTimeout, commandType);
+            if (!(cmd is SqlCommand))
+                throw new NotSupportedException("Async support currently only available for SqlCommand.");
+            
+            var sqlCmd = (SqlCommand)cmd;
+
+            using (var reader = await sqlCmd.ExecuteReaderAsync())
+            {
+                return ExecuteReader<T>(reader, identity, info).ToList();
+            }
+        }
+
+        private static IEnumerable<T> ExecuteReader<T>(IDataReader reader, Identity identity, CacheInfo info)
+        {
+            var tuple = info.Deserializer;
+            int hash = GetColumnHash(reader);
+            if (tuple.Func == null || tuple.Hash != hash)
+            {
+                tuple = info.Deserializer = new DeserializerState(hash, GetDeserializer(typeof(T), reader, 0, -1, false));
+                SetQueryCache(identity, info);
+            }
+
+            var func = tuple.Func;
+
+            while (reader.Read())
+            {
+                yield return (T)func(reader);
+            }
+        }
+    }
+}
---
+++ DapperTests NET45/Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Dapper;
+using SqlMapper;
+
+namespace DapperTests_NET45
+{
+    public class Tests
+    {
+        public void AsyncTestBasicStringUsage()
+        {
+            using (var connection = Program.GetOpenConnection())
+            {
+                var query = connection.QueryAsync<string>("select 'abc' as [Value] union all select @txt", new {txt = "def"});
+                var arr = query.Result.ToArray();
+                arr.IsSequenceEqualTo(new[] {"abc", "def"});
+            }
+        }
+        
+        public void AsyncTestClassWithStringUsage()
+        {
+            using (var connection = Program.GetOpenConnection())
+            {
+                var query = connection.QueryAsync<BasicType>("select 'abc' as [Value] union all select @txt", new { txt = "def" });
+                var arr = query.Result.ToArray();
+                arr.Select(x => x.Value).IsSequenceEqualTo(new[] { "abc", "def" });    
+            }
+        }
+        
+        class BasicType
+        { 
+            public string Value { get; set; }
+        }
+    }
+}
