d4faf6973060ff43621b4e9676982b19703d9680
bkaid <brandonkaid@gmail.com>
2012-09-14 12:26:28 -0700

    Added async versions of Query methods that do multi-mapping like QueryAsync<TFirst, TSecond, TReturn>

--- Dapper NET45/SqlMapperAsync.cs
+++ Dapper NET45/SqlMapperAsync.cs
@@ -16,16 +16,113 @@ namespace Dapper
         {
             var identity = new Identity(sql, commandType, cnn, typeof(T), param == null ? null : param.GetType(), null);
             var info = GetCacheInfo(identity);
+            var cmd = (DbCommand)SetupCommand(cnn, transaction, sql, info.ParamReader, param, commandTimeout, commandType);
 
-            var cmd = SetupCommand(cnn, transaction, sql, info.ParamReader, param, commandTimeout, commandType);
-            if (!(cmd is DbCommand))
-                throw new NotSupportedException("Command must be a DbCommand.");
-
-            var dbCmd = (DbCommand)cmd;
-
-            using (var reader = await dbCmd.ExecuteReaderAsync())
+            using (var reader = await cmd.ExecuteReaderAsync())
             {
                 return ExecuteReader<T>(reader, identity, info).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Maps a query to objects
+        /// </summary>
+        /// <typeparam name="TFirst">The first type in the recordset</typeparam>
+        /// <typeparam name="TSecond">The second type in the recordset</typeparam>
+        /// <typeparam name="TReturn">The return type</typeparam>
+        /// <param name="cnn"></param>
+        /// <param name="sql"></param>
+        /// <param name="map"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <param name="splitOn">The Field we should split and read the second object from (default: id)</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout</param>
+        /// <param name="commandType">Is it a stored proc or a batch?</param>
+        /// <returns></returns>
+        public static async Task<IEnumerable<TReturn>> QueryAsync<TFirst, TSecond, TReturn>(this IDbConnection cnn, string sql, Func<TFirst, TSecond, TReturn> map, dynamic param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return await MultiMapAsync<TFirst, TSecond, DontMap, DontMap, DontMap, TReturn>(cnn, sql, map, param as object, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        /// <summary>
+        /// Maps a query to objects
+        /// </summary>
+        /// <typeparam name="TFirst"></typeparam>
+        /// <typeparam name="TSecond"></typeparam>
+        /// <typeparam name="TThird"></typeparam>
+        /// <typeparam name="TReturn"></typeparam>
+        /// <param name="cnn"></param>
+        /// <param name="sql"></param>
+        /// <param name="map"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <param name="splitOn">The Field we should split and read the second object from (default: id)</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout</param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static async Task<IEnumerable<TReturn>> QueryAsync<TFirst, TSecond, TThird, TReturn>(this IDbConnection cnn, string sql, Func<TFirst, TSecond, TThird, TReturn> map, dynamic param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return await MultiMapAsync<TFirst, TSecond, TThird, DontMap, DontMap, TReturn>(cnn, sql, map, param as object, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        /// <summary>
+        /// Perform a multi mapping query with 4 input parameters
+        /// </summary>
+        /// <typeparam name="TFirst"></typeparam>
+        /// <typeparam name="TSecond"></typeparam>
+        /// <typeparam name="TThird"></typeparam>
+        /// <typeparam name="TFourth"></typeparam>
+        /// <typeparam name="TReturn"></typeparam>
+        /// <param name="cnn"></param>
+        /// <param name="sql"></param>
+        /// <param name="map"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <param name="splitOn"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static async Task<IEnumerable<TReturn>> QueryAsync<TFirst, TSecond, TThird, TFourth, TReturn>(this IDbConnection cnn, string sql, Func<TFirst, TSecond, TThird, TFourth, TReturn> map, dynamic param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return await MultiMapAsync<TFirst, TSecond, TThird, TFourth, DontMap, TReturn>(cnn, sql, map, param as object, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        /// <summary>
+        /// Perform a multi mapping query with 5 input parameters
+        /// </summary>
+        /// <typeparam name="TFirst"></typeparam>
+        /// <typeparam name="TSecond"></typeparam>
+        /// <typeparam name="TThird"></typeparam>
+        /// <typeparam name="TFourth"></typeparam>
+        /// <typeparam name="TFifth"></typeparam>
+        /// <typeparam name="TReturn"></typeparam>
+        /// <param name="cnn"></param>
+        /// <param name="sql"></param>
+        /// <param name="map"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <param name="splitOn"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static async Task<IEnumerable<TReturn>> QueryAsync<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(this IDbConnection cnn, string sql, Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> map, dynamic param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return await MultiMapAsync<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(cnn, sql, map, param as object, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        static async Task<IEnumerable<TReturn>> MultiMapAsync<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(this IDbConnection cnn, string sql, object map, object param, IDbTransaction transaction, bool buffered, string splitOn, int? commandTimeout, CommandType? commandType)
+        {
+            var identity = new Identity(sql, commandType, cnn, typeof(TFirst), (object)param == null ? null : ((object)param).GetType(), new[] { typeof(TFirst), typeof(TSecond), typeof(TThird), typeof(TFourth), typeof(TFifth) });
+            var info = GetCacheInfo(identity);
+            var cmd = (DbCommand)SetupCommand(cnn, transaction, sql, info.ParamReader, param, commandTimeout, commandType);
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                var results = MultiMapImpl<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(null, null, map, null, null, splitOn, null, null, reader, identity);
+                return buffered ? results.ToList() : results;
             }
         }
 
--- DapperTests NET45/Tests.cs
+++ DapperTests NET45/Tests.cs
@@ -6,28 +6,61 @@ namespace DapperTests_NET45
 {
     public class Tests
     {
-        public void AsyncTestBasicStringUsage()
+        public void TestBasicStringUsageAsync()
         {
             using (var connection = Program.GetOpenConnection())
             {
-                var query = connection.QueryAsync<string>("select 'abc' as [Value] union all select @txt", new {txt = "def"});
+                var query = connection.QueryAsync<string>("select 'abc' as [Value] union all select @txt", new { txt = "def" });
                 var arr = query.Result.ToArray();
-                arr.IsSequenceEqualTo(new[] {"abc", "def"});
+                arr.IsSequenceEqualTo(new[] { "abc", "def" });
             }
         }
-        
-        public void AsyncTestClassWithStringUsage()
+
+        public void TestClassWithStringUsageAsync()
         {
             using (var connection = Program.GetOpenConnection())
             {
                 var query = connection.QueryAsync<BasicType>("select 'abc' as [Value] union all select @txt", new { txt = "def" });
                 var arr = query.Result.ToArray();
-                arr.Select(x => x.Value).IsSequenceEqualTo(new[] { "abc", "def" });    
+                arr.Select(x => x.Value).IsSequenceEqualTo(new[] { "abc", "def" });
             }
         }
-        
+
+        public void TestMultiMapWithSplitAsync()
+        {
+            var sql = @"select 1 as id, 'abc' as name, 2 as id, 'def' as name";
+            using (var connection = Program.GetOpenConnection())
+            {
+                var productQuery = connection.QueryAsync<Product, Category, Product>(sql, (prod, cat) =>
+                {
+                    prod.Category = cat;
+                    return prod;
+                });
+
+                var product = productQuery.Result.First();
+                // assertions
+                product.Id.IsEqualTo(1);
+                product.Name.IsEqualTo("abc");
+                product.Category.Id.IsEqualTo(2);
+                product.Category.Name.IsEqualTo("def");
+            }
+        }
+
+        class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Category Category { get; set; }
+        }
+        class Category
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
         class BasicType
-        { 
+        {
             public string Value { get; set; }
         }
     }
