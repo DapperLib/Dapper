d462d7d30b125d198e73456527edb59f9c74bd35
Marc Gravell <marc.gravell@gmail.com>
2012-09-19 07:31:59 +0100

    1.12 deploy
    Signed-off-by: Marc Gravell <marc.gravell@gmail.com>

--- Dapper/Properties/AssemblyInfo.cs
+++ Dapper/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.0.0")]
-[assembly: AssemblyFileVersion("1.11.0.0")]
+[assembly: AssemblyVersion("1.12.0.0")]
+[assembly: AssemblyFileVersion("1.12.0.0")]
