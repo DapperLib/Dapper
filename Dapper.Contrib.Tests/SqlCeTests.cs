4b9e7fad9039b762e88119ea17761aef629d040a
Scott Heighway <scotlord80@gmail.com>
2012-10-11 13:10:26 +1300

    Can now insert an object with a [Key] attribute without needing a default value on the DB column.
    Added an optional parameter for specifying that the value provided with the [Key] Parameter should be used when performing the insert.  This allows primary key indication (for updates) on tables with no default value for the primary key.
    
    Added a test for the feature added above.  It only runs for Postgres, as SqlCe gets angry about no IDENTITY on Id.
    
    Added the ability to run the tests against different databases for Dapper.Contrib, and implemented running against a Postgres database.  As there is no 'in memory' Postgres feature, so the user will have to set up their own copy.  Due to that, it's commented out by default.

---
+++ Dapper.Contrib.Tests/SqlCeTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Dapper.Contrib.Tests
+{
+    class SqlCeTests : Tests
+    {
+        private readonly string _assemblyLocation;
+        private readonly string _projFolder;
+        private bool _setUp;
+
+        public SqlCeTests()
+        {
+            _assemblyLocation = Assembly.GetAssembly(GetType()).Location;
+            _projFolder = Path.GetDirectoryName(_assemblyLocation);
+        }
+
+        protected override IDbConnection GetOpenConnection()
+        {
+            var connection = new SqlCeConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+
+        protected override string ConnectionString
+        {
+            get
+            {
+                return "Data Source = " + _projFolder + "\\Test.sdf;";
+            }
+        }
+
+        public override void SetUpTests()
+        {
+            if (_setUp)
+                return;
+
+            if (File.Exists(_projFolder + "\\Test.sdf"))
+                File.Delete(_projFolder + "\\Test.sdf");
+            var connectionString = "Data Source = " + _projFolder + "\\Test.sdf;";
+            var engine = new SqlCeEngine(connectionString);
+            engine.CreateDatabase();
+
+            using (var connection = new SqlCeConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute(@" create table Users (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null, Age int not null) ");
+                connection.Execute(@" create table Automobiles (Id int IDENTITY(1,1) not null, Name nvarchar(100) not null) ");
+                connection.Execute(@" create table Houses (Id int not null, Number int not null, Road nvarchar(100) not null) ");
+            }
+            _setUp = true;
+        }
+    }
+}
