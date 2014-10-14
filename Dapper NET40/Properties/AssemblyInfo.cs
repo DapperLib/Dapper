d9f2801f0d587cb3a5d1a39ea40381fd4d1664e3
Marc Gravell <marc.gravell@gmail.com>
2014-10-14 15:49:53 +0100

    1.36 release

--- Dapper NET40/Properties/AssemblyInfo.cs
+++ Dapper NET40/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.35.0.0")]
-[assembly: AssemblyFileVersion("1.35.0.0")]
+[assembly: AssemblyVersion("1.36.0.0")]
+[assembly: AssemblyFileVersion("1.36.0.0")]
