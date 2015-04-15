99fdd2242bd76972abf4b7eacd059afbd81bb971
Obalix <axel@eckenberger.de>
2015-04-15 11:41:27 +0200

    Added DapperSqlBuilderMixin to simplify using SqlBuilder.Templates with the Dapper Query method.

---
+++ Dapper.SqlBuilder/DapperSqlBuilderMixin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Dapper
+{
+	public static class DapperSqlBuilderMixin
+	{
+		public static IEnumerable<TReturn> Query<TReturn>(
+			this IDbConnection cnn, 
+			SqlBuilder.Template query, 
+			IDbTransaction transaction = null, 
+			bool buffered = true, 
+			int? commandTimeout = null, 
+			CommandType? commandType = null
+		)
+		{
+			if (query == null) throw new ArgumentNullException("query");
+
+			return cnn.Query<TReturn>(query.RawSql, query.Parameters);
+		}
+	}
+}
