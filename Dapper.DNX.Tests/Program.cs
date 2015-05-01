a161cb5aca103b8c784ed7089fb8b751c22ae9c7
Marc Gravell <marc.gravell@gmail.com>
2015-05-01 21:18:51 +0100

    CoreCLR build / 1.41-beta

--- Dapper.DNX.Tests/Program.cs
+++ Dapper.DNX.Tests/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Dapper.DNX.Tests
 {
@@ -9,7 +10,11 @@ namespace Dapper.DNX.Tests
     {
         public void Main()
         {
+#if DNXCORE50
+            Console.WriteLine("From: {0}", typeof(int).AssemblyQualifiedName);
+#else
             Console.WriteLine("Version: {0}", Environment.Version);
+#endif
             const string connectionString = "Data Source=.;Initial Catalog=tempdb;Integrated Security=True";
             using (var conn = new SqlConnection(connectionString))
             {
@@ -18,15 +23,11 @@ namespace Dapper.DNX.Tests
                 Console.WriteLine(row.X);
 
                 var methods = typeof(Dapper.SqlMapper).GetMethods().Where(x => x.Name == "QueryAsync").ToList();
-
+#if ASYNC
                 row = conn.QueryAsync<Foo>("select @a as X", new { a = 123 }).Result.Single();
+#endif
                 Console.WriteLine(row.X);
             }
-        }
-        private static async Task<int> WithDelay(int i)
-        {
-            await Task.Delay(100);
-            return i;
         }
         class Foo
         {
