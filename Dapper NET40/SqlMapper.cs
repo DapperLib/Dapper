896cac1906878e0821b3ab3d7bce3abfc22f0cc8
Jesse <jesse@orangecoding.com>
2015-05-12 13:13:04 -0400

    Added a static class, Settings, to the SqlMapper class. The Settings class will hold some "global" settings, which the SqlMapper will use.

--- Dapper NET40/SqlMapper.cs
+++ Dapper NET40/SqlMapper.cs
@@ -253,6 +253,30 @@ namespace Dapper
     static partial class SqlMapper
     {
         /// <summary>
+        /// Permits specifying certain SqlMapper values globally.
+        /// </summary>
+        public static class Settings
+        {
+            static Settings()
+            {
+                SetDefaults();
+            }
+
+            /// <summary>
+            /// Resets all Settings to their default values
+            /// </summary>
+            public static void SetDefaults()
+            {
+                CommandTimeout = null;
+            }
+
+            /// <summary>
+            /// Specifies the default Command Timeout for all Queries
+            /// </summary>
+            public static int? CommandTimeout { get; set; }
+        }
+
+        /// <summary>
         /// Implement this interface to pass an arbitrary db specific set of parameters to Dapper
         /// </summary>
         public partial interface IDynamicParameters
