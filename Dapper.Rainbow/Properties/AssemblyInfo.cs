e68342112e1bd49f7ea6c63fa5d0c6323a4dfb6f
Luke McGregor <luke@mcgregor.org.nz>
2015-07-14 09:41:18 +1200

    Aligned nuspec and assembly versions and used token replace to keep consistant

--- Dapper.Rainbow/Properties/AssemblyInfo.cs
+++ Dapper.Rainbow/Properties/AssemblyInfo.cs
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
--- Dapper.SqlBuilder/Properties/AssemblyInfo.cs
+++ Dapper.SqlBuilder/Properties/AssemblyInfo.cs
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.40.0.0")]
 [assembly: AssemblyFileVersion("1.40.0.0")]
+[assembly: AssemblyInformationalVersion("1.40.0")]
