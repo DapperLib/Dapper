fa03d54b29227bda6bd670fd024b742a49d8b581
Marc Gravell <marc.gravell@gmail.com>
2015-12-24 11:41:07 +0000

    Attempt (unsuccessful) to repro #426

--- Dapper.Tests/Tests.cs
+++ Dapper.Tests/Tests.cs
@@ -2942,7 +2942,35 @@ end");
         {
             using (var conn = GetMySqlConnection(true, true, true)) { TestDateTime(connection); }
         }
+        [FactMySql]
+        public void Issue426_SO34439033_DateTimeGainsTicks()
+        {
+            using (var conn = GetMySqlConnection())
+            {
+                try { conn.Execute("drop table Issue426_Test"); } catch { }
+                try { conn.Execute("create table Issue426_Test (Id int not null, Time time not null)"); } catch { }
+                const long ticks = 553440000000;
+                const int Id = 426;
+               
+                var localObj = new Issue426_Test
+                {
+                    Id = Id,
+                    Time = TimeSpan.FromTicks(ticks) // from code example
+                };
+                conn.Execute("replace into Issue426_Test values (@Id,@Time)", localObj);
+                
+                var dbObj = conn.Query<Issue426_Test>("select * from Issue426_Test where Id = @id", new { id = Id }).Single();
+                dbObj.Id.IsEqualTo(Id);
+                dbObj.Time.Value.Ticks.IsEqualTo(ticks);
+                
+            }
+        }
 
+        public class Issue426_Test
+        {
+            public long Id { get; set; }
+            public TimeSpan? Time { get; set; }
+        }
         public class FactMySqlAttribute : FactAttribute
         {
             public override string Skip
