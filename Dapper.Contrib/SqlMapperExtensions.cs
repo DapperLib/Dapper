21d53445046491211855301833f9f27ae00b8522
Luke <rt.luke.s@gmail.com>
2016-03-26 01:25:50 +1000

    [Postgres] RETURNING should be in the same format as other columns

--- Dapper.Contrib/SqlMapperExtensions.cs
+++ Dapper.Contrib/SqlMapperExtensions.cs
@@ -810,7 +810,7 @@ public partial class PostgresAdapter : ISqlAdapter
                 if (!first)
                     sb.Append(", ");
                 first = false;
-                sb.Append(property.Name);
+                AppendColumnName(sb, property.Name);
             }
         }
 
