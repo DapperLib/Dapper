d506c176b0978acf2005bff91f026685e5256474
Marc Gravell <marc.gravell@gmail.com>
2016-09-08 10:42:01 +0100

    failing test for #563

--- Dapper.Tests/Tests.Async.cs
+++ Dapper.Tests/Tests.Async.cs
@@ -771,6 +771,19 @@ SET @AddressPersonId = @PersonId", p))
                 reader.Read().IsFalse();
             }
         }
+
+        [Fact]
+        public async Task Issue563_QueryAsyncShouldThrowException()
+        {
+            try
+            {
+                var data = (await connection.QueryAsync<int>("select 1 union all select 2")).ToList();
+                Assert.Fail();
+            }catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
 #endif
