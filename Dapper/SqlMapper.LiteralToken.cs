e65c193d26ad39ce4bdf2663be8b692a203e83d4
fred <fredli.ex@gmail.com>
2016-11-15 00:24:43 +0800

    make CreateParamInfoGenerator and GetTypeDeserializerImpl can be customized

--- Dapper/SqlMapper.LiteralToken.cs
+++ Dapper/SqlMapper.LiteralToken.cs
@@ -7,7 +7,7 @@ namespace Dapper
         /// <summary>
         /// Represents a placeholder for a value that should be replaced as a literal value in the resulting sql
         /// </summary>
-        internal struct LiteralToken
+        public struct LiteralToken
         {
             /// <summary>
             /// The text in the original command that should be replaced
