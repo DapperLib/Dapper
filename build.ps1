690e0c325de6773caccd9d39c92f3f37e8d60d07
Sergii Volchkov <Sergii.Volchkov@betssongroup.com>
2017-07-04 16:33:01 +0200

    Disable parallel test run by assembly attribute.

--- build.ps1
+++ build.ps1
@@ -72,7 +72,7 @@ if ($RunTests) {
         Write-Host "Running tests: $project (all frameworks)" -ForegroundColor "Magenta"
         Push-Location "$project"
 
-        dotnet xunit -parallel none
+        dotnet xunit
         if ($LastExitCode -ne 0) { 
             Write-Host "Error with tests, aborting build." -Foreground "Red"
             Pop-Location
