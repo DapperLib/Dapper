6bcd3bd7f82c831c374ccea4cd297e55e1c1b074
BeanHsiang <bin.xiang@xiaobao100.com>
2018-01-25 15:30:53 +0800

    created DbManager object

---
+++ Dapper.Contrib/DbManager.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Dapper.Contrib.Extensions
+{
+    enum DbActionType
+    {
+        Insert,
+        Update,
+        Delete,
+        DeleteAll,
+        //Get,
+        //GetAll
+    }
+
+    class DbAction<T> where T : class
+    {
+        public DbActionType ActionType { get; }
+
+        public T Entity { get; }
+
+        public DbAction(DbActionType actionType, T entity)
+        {
+            ActionType = actionType;
+            Entity = entity;
+        }
+    }
+
+    /// <summary>
+    /// The manager wrapped IDbConnection
+    /// </summary>
+    public class DbManager
+    {
+        private Lazy<IDbConnection> internalConnection;
+        private IDbTransaction internalDbTransaction = null;
+        private bool isOpened = false;
+        private Queue DbActions { get; } = new Queue(8);
+        private static readonly ConcurrentDictionary<RuntimeTypeHandle, Delegate> insertDelegates = new ConcurrentDictionary<RuntimeTypeHandle, Delegate>();
+        private static readonly ConcurrentDictionary<RuntimeTypeHandle, Delegate> updateDelegates = new ConcurrentDictionary<RuntimeTypeHandle, Delegate>();
+        private static readonly ConcurrentDictionary<RuntimeTypeHandle, Delegate> deleteDelegates = new ConcurrentDictionary<RuntimeTypeHandle, Delegate>();
+        private static readonly ConcurrentDictionary<RuntimeTypeHandle, Delegate> getDelegates = new ConcurrentDictionary<RuntimeTypeHandle, Delegate>();
+
+        public DbManager(Func<IDbConnection> factory) => internalConnection = new Lazy<IDbConnection>(factory, true);
+
+        public void Insert<T>(T entity) where T : class
+        {
+            DbActions.Enqueue(new DbAction<T>(DbActionType.Insert, entity));
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            DbActions.Enqueue(new DbAction<T>(DbActionType.Update, entity));
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            DbActions.Enqueue(new DbAction<T>(DbActionType.Delete, entity));
+        }
+
+        public T Get<T>(dynamic id)
+        {
+            var entityType = typeof(T);
+            var expr = GetGetDelegate(entityType);
+            return (T)expr.DynamicInvoke(GetConnection(), id, null, null);
+        }
+
+        private void OpenConnection()
+        {
+            var con = GetConnection();
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            var con = GetConnection();
+            if (con.State != ConnectionState.Closed)
+            {
+                con.Close();
+            }
+        }
+
+        public void BeginTrasaction()
+        {
+            if (internalDbTransaction == null)
+            {
+                OpenConnection();
+                var con = GetConnection();
+                internalDbTransaction = con.BeginTransaction();
+            }
+        }
+
+        public void Commit()
+        {
+            if (internalDbTransaction != null)
+            {
+                internalDbTransaction.Commit();
+            }
+
+            if (!isOpened)
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Rollback()
+        {
+            if (internalDbTransaction != null)
+            {
+                internalDbTransaction.Rollback();
+            }
+
+            if (!isOpened)
+            {
+                CloseConnection();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            while (DbActions.Count > 0)
+            {
+                var action = DbActions.Dequeue();
+                InvokeAction(action);
+            }
+        }
+
+        private dynamic InvokeAction(object action)
+        {
+            Type dbActionType = action.GetType();
+
+            var entityType = dbActionType.GenericTypeArguments[0];
+            var entity = GetEntityOfAction(action);
+            if (entityType != entity.GetType())
+            {
+                throw new InvalidOperationException("");
+
+            }
+
+            var actionType = GetActionTypeOfAction(action);
+            Delegate expr;
+            switch (actionType)
+            {
+                case DbActionType.Insert:
+                    expr = GetInsertDelegate(dbActionType, entityType);
+                    expr.DynamicInvoke(GetConnection(), entity, null, null);
+                    break;
+                case DbActionType.Update:
+                    expr = GetUpdateDelegate(dbActionType, entityType);
+                    expr.DynamicInvoke(GetConnection(), entity, null, null);
+                    break;
+                case DbActionType.Delete:
+                    expr = GetDeleteDelegate(dbActionType, entityType);
+                    expr.DynamicInvoke(GetConnection(), entity, null, null);
+                    break;
+                case DbActionType.DeleteAll:
+                    break;
+                default:
+                    break;
+            }
+
+            return null;
+        }
+
+        private Delegate GetInsertDelegate(Type dbActionType, Type entityType)
+        {
+            if (insertDelegates.TryGetValue(dbActionType.TypeHandle, out Delegate insertDelegate))
+            {
+                return insertDelegate;
+            }
+            var method = typeof(SqlMapperExtensions).GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).FirstOrDefault(x => x.Name == "Insert" && x.IsGenericMethodDefinition).MakeGenericMethod(dbActionType.GetGenericArguments());
+            var param0 = Expression.Parameter(typeof(IDbConnection));
+            var param1 = Expression.Parameter(entityType);
+            var param2 = Expression.Parameter(typeof(IDbTransaction));
+            var param3 = Expression.Parameter(typeof(int?));
+            var source = Expression.Call(method, param0, param1, param2, param3);
+
+            var expr = Expression.Lambda(source, param0, param1, param2, param3).Compile();
+            insertDelegates[dbActionType.TypeHandle] = expr;
+            return expr;
+        }
+
+        private Delegate GetUpdateDelegate(Type dbActionType, Type entityType)
+        {
+            if (updateDelegates.TryGetValue(dbActionType.TypeHandle, out Delegate updateDelegate))
+            {
+                return updateDelegate;
+            }
+            var method = typeof(SqlMapperExtensions).GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).FirstOrDefault(x => x.Name == "Update" && x.IsGenericMethodDefinition).MakeGenericMethod(dbActionType.GetGenericArguments());
+            var param0 = Expression.Parameter(typeof(IDbConnection));
+            var param1 = Expression.Parameter(entityType);
+            var param2 = Expression.Parameter(typeof(IDbTransaction));
+            var param3 = Expression.Parameter(typeof(int?));
+            var source = Expression.Call(method, param0, param1, param2, param3);
+
+            var expr = Expression.Lambda(source, param0, param1, param2, param3).Compile();
+            updateDelegates[dbActionType.TypeHandle] = expr;
+            return expr;
+        }
+
+        private Delegate GetDeleteDelegate(Type dbActionType, Type entityType)
+        {
+            if (deleteDelegates.TryGetValue(dbActionType.TypeHandle, out Delegate deleteDelegate))
+            {
+                return deleteDelegate;
+            }
+            var method = typeof(SqlMapperExtensions).GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).FirstOrDefault(x => x.Name == "Delete" && x.IsGenericMethodDefinition).MakeGenericMethod(dbActionType.GetGenericArguments());
+            var param0 = Expression.Parameter(typeof(IDbConnection));
+            var param1 = Expression.Parameter(entityType);
+            var param2 = Expression.Parameter(typeof(IDbTransaction));
+            var param3 = Expression.Parameter(typeof(int?));
+            var source = Expression.Call(method, param0, param1, param2, param3);
+
+            var expr = Expression.Lambda(source, param0, param1, param2, param3).Compile();
+            deleteDelegates[dbActionType.TypeHandle] = expr;
+            return expr;
+        }
+
+        private Delegate GetGetDelegate(Type entityType)
+        {
+            if (getDelegates.TryGetValue(entityType.TypeHandle, out Delegate getDelegate))
+            {
+                return getDelegate;
+            }
+            var methodInfo = typeof(SqlMapperExtensions).GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).FirstOrDefault(x => x.Name == "Get" && x.IsGenericMethodDefinition);
+            var method = methodInfo.MakeGenericMethod(new Type[] { entityType });
+            var parameterInfo = methodInfo.GetParameters().FirstOrDefault(p => p.Name.Equals("id", StringComparison.OrdinalIgnoreCase));
+
+            var param0 = Expression.Parameter(typeof(IDbConnection));
+            var PropertyInfoOfEntityDelegate = GetKeyPropertyInfoOfEntityDelegate(entityType);
+            var keyPropertyInfo = PropertyInfoOfEntityDelegate.Invoke(nameof(DbManager), nameof(GetGetDelegate));
+            var param1 = Expression.Parameter(parameterInfo.ParameterType);
+            var param2 = Expression.Parameter(typeof(IDbTransaction));
+            var param3 = Expression.Parameter(typeof(int?));
+            var source = Expression.Call(method, param0, param1, param2, param3);
+
+            var expr = Expression.Lambda(source, param0, param1, param2, param3).Compile();
+            getDelegates[entityType.TypeHandle] = expr;
+            return expr;
+        }
+
+        private Func<string, string, PropertyInfo> GetKeyPropertyInfoOfEntityDelegate(Type entityType)
+        {
+            var method = typeof(SqlMapperExtensions).GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).FirstOrDefault(x => x.Name == "GetSingleKeyOfEntity" && x.IsGenericMethodDefinition).MakeGenericMethod(new Type[] { entityType });
+            var param0 = Expression.Parameter(typeof(string));
+            var param1 = Expression.Parameter(typeof(string));
+            var source = Expression.Call(method, param0, param1);
+
+            var expr = Expression.Lambda<Func<string, string, PropertyInfo>>(source, param0, param1).Compile();
+            return expr;
+        }
+
+        private object GetEntityOfAction(object obj)
+        {
+            var memberExpr = Expression.PropertyOrField(Expression.Constant(obj), "Entity");
+            return Expression.Lambda(memberExpr).Compile().DynamicInvoke();
+        }
+
+        private DbActionType GetActionTypeOfAction(object obj)
+        {
+            var memberExpr = Expression.PropertyOrField(Expression.Constant(obj), "ActionType");
+            return (DbActionType)Expression.Lambda(memberExpr).Compile().DynamicInvoke();
+        }
+
+        private IDbConnection GetConnection()
+        {
+            var isFirstCreated = !internalConnection.IsValueCreated;
+            var con = internalConnection.Value;
+            if (isFirstCreated)
+            {
+                isOpened = con.State == ConnectionState.Open;
+                //CloseConnection();
+            }
+            return con;
+        }
+    }
+}
---
+++ Dapper.Tests.Contrib/TestDbManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+using Xunit;
+
+namespace Dapper.Tests.Contrib
+{
+    enum Gender
+    {
+        Male = 0,
+        Female
+    }
+
+    [Table("person")]
+    class Human
+    {
+        public long Id { get; set; }
+        protected string Region { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public bool Enabled { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+
+    public class TestDbManager
+    {
+        const string conStr = "server=LocalHost;User Id=root;Pwd=greedyint;database=test";
+        Random rand = new Random();
+
+        [Fact]
+        public void TestSaveChanges()
+        {
+            var mgr = new DbManager(() => { return new MySqlConnection(conStr); });
+
+            var person = new Human()
+            {
+                Id = 11,
+                Name = "TestDbManager",
+                Age = rand.Next(1, 100),
+                Address = "TestInsertStrongClassInstanceAddress",
+                Enabled = true
+            };
+            mgr.Insert(person);
+            mgr.SaveChanges();
+
+            var queryPerson = mgr.Get<Human>(person.Id);
+            Assert.Equal(person.Age, queryPerson.Age);
+        }
+    }
+}
