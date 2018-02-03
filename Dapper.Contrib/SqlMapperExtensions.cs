1b2e8116c7e426bb49b7977ce0019f38d942e051
Jason <jase_randall@hotmail.com>
2018-02-03 14:45:11 +1100

    Change to GetAll to allow the usage of a nullable type in T when T is an interface.

--- Dapper.Contrib/SqlMapperExtensions.cs
+++ Dapper.Contrib/SqlMapperExtensions.cs
@@ -249,7 +249,16 @@ namespace Dapper.Contrib.Extensions
                 foreach (var property in TypePropertiesCache(type))
                 {
                     var val = res[property.Name];
-                    property.SetValue(obj, Convert.ChangeType(val, property.PropertyType), null);
+                    if (property.PropertyType.IsGenericType() && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    {
+                        if (val == null) continue;
+                        var genericType = property.PropertyType.GetGenericArguments()[0];
+                        property.SetValue(obj, Convert.ChangeType(val, genericType), null);
+                    }
+                    else
+                    {
+                        property.SetValue(obj, Convert.ChangeType(val, property.PropertyType), null);
+                    }
                 }
                 ((IProxy)obj).IsDirty = false;   //reset change tracking and return
                 list.Add(obj);
