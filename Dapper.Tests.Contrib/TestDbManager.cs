60190dd7f408c8f054d6d58040e8f27bfe876f56
BeanHsiang <bin.xiang@xiaobao100.com>
2018-02-08 15:03:58 +0800

    added unit tests for DbManager

--- Dapper.Tests.Contrib/TestDbManager.cs
+++ Dapper.Tests.Contrib/TestDbManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using Dapper.Contrib.Extensions;
 using MySql.Data.MySqlClient;
 using Xunit;
@@ -24,9 +26,10 @@ namespace Dapper.Tests.Contrib
         public DateTime? CreatedDate { get; set; }
     }
 
+
     public class TestDbManager
     {
-        const string conStr = "server=LocalHost;User Id=root;Pwd=greedyint;database=test";
+        const string conStr = "server=LocalHost;User Id=root;Pwd=MYPASSWORD;database=test";
         Random rand = new Random();
 
         [Fact]
@@ -48,5 +51,73 @@ namespace Dapper.Tests.Contrib
             var queryPerson = mgr.Get<Human>(person.Id);
             Assert.Equal(person.Age, queryPerson.Age);
         }
+
+
+        [Fact]
+        public void TestGetMapper()
+        {
+            var mgr = new DbManager(() => { return new MySqlConnection(conStr); });
+
+            var person = new Human()
+            {
+                Id = 11,
+                Name = "TestDbManager",
+                Age = rand.Next(1, 100),
+                Address = "TestInsertStrongClassInstanceAddress",
+                Enabled = true
+            };
+
+            var mapper = mgr.GetMapper<ISpUserMapper>();
+            //var mapper2 = new SpUserMapper2(new MySqlConnection(conStr));
+
+            //var arr2 = mapper2.GetUserNames(2);
+            int a = mapper.UpdateUsers("MapperUpdated" + rand.Next(1, 100), 2);
+            var arr = mapper.GetUserNames(2);
+        }
     }
+
+
+    public interface ISpUserMapper : ISqlOperationMapper
+    {
+        [QuerySql("select name from person where age>@age")]
+        string[] GetUserNames(int age);
+
+        [QuerySql("select name from person where age>@age")]
+        IEnumerable<string> GetUserNames2(int age);
+
+        [ExcuteSql("update person set address=@address where age>@age")]
+        int UpdateUsers(string address, int age);
+    }
+
+    //public class SpUserMapper2 : ISpUserMapper
+    //{
+    //    private IDbConnection connection;
+
+    //    public SpUserMapper2(IDbConnection connection) => this.connection = connection;
+
+    //    public string[] GetUserNames(int age)
+    //    {
+    //        var sql = "select * from person where age>@age";
+    //        var dic = new Dictionary<string, dynamic>();
+    //        dic.Add("age", age);
+    //        return connection.Query<string>(sql, dic).ToArray();
+    //    }
+
+    //    public IEnumerable<string> GetUserNames2(int age)
+    //    {
+    //        var sql = "select * from person where age>@age";
+    //        var dic = new Dictionary<string, dynamic>();
+    //        dic.Add("age", age);
+    //        return connection.Query<string>(sql, dic);
+    //    }
+
+    //    public int UpdateUsers(string address, int age)
+    //    {
+    //        var sql = "111";
+    //        var dic = new Dictionary<string, dynamic>();
+    //        dic.Add("address", address);
+    //        dic.Add("age", age);
+    //        return connection.Execute(sql, dic);
+    //    }
+    //}
 }
