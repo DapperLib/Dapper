494a68ecfca5c16aacabfdad2030e936ff076f19
BeanHsiang <bin.xiang@xiaobao100.com>
2018-02-09 10:35:49 +0800

    fixed TestDbManager facts

--- Dapper.Tests.Contrib/TestDbManager.cs
+++ Dapper.Tests.Contrib/TestDbManager.cs
@@ -7,117 +7,49 @@ using Xunit;
 
 namespace Dapper.Tests.Contrib
 {
-    enum Gender
+    public interface ISpUserMapper : ISqlOperationMapper
     {
-        Male = 0,
-        Female
+        [QuerySql("select name from `User` where age>@age")]
+        string[] GetUserNames(int age);
+
+        [ExcuteSql("update `User` set name=@name where age>@age")]
+        int UpdateUsers(string name, int age);
     }
 
-    [Table("person")]
-    class Human
+    public abstract partial class TestSuite
     {
-        public long Id { get; set; }
-        protected string Region { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Address { get; set; }
-        public bool Enabled { get; set; }
-        public Gender Gender { get; set; }
-        public DateTime? CreatedDate { get; set; }
-    }
-
-
-    public class TestDbManager
-    {
-        const string conStr = "server=LocalHost;User Id=root;Pwd=MYPASSWORD;database=test";
         Random rand = new Random();
 
         [Fact]
         public void TestSaveChanges()
         {
-            var mgr = new DbManager(() => { return new MySqlConnection(conStr); });
+            var mgr = new DbManager(() => { return GetConnection(); });
 
-            var person = new Human()
+            var user = new User()
             {
-                Id = 11,
-                Name = "TestDbManager",
-                Age = rand.Next(1, 100),
-                Address = "TestInsertStrongClassInstanceAddress",
-                Enabled = true
+                Name = "TestDbManagerName",
+                Age = rand.Next(1, 100)
             };
-            mgr.Insert(person);
+            mgr.Insert(user);
             mgr.SaveChanges();
 
-            var queryPerson = mgr.Get<Human>(person.Id);
-            Assert.Equal(person.Age, queryPerson.Age);
+            var queryUser = mgr.Get<User>(user.Id);
+            Assert.Equal(user.Age, queryUser.Age);
         }
 
 
         [Fact]
         public void TestGetMapper()
         {
-            var mgr = new DbManager(() => { return new MySqlConnection(conStr); });
-
-            var person = new Human()
-            {
-                Id = 11,
-                Name = "TestDbManager",
-                Age = rand.Next(1, 100),
-                Address = "TestInsertStrongClassInstanceAddress",
-                Enabled = true
-            };
-
+            var mgr = new DbManager(() => { return GetConnection(); });
             var mapper = mgr.GetMapper<ISpUserMapper>();
-            //var mapper2 = new SpUserMapper2(new MySqlConnection(conStr));
-
-            //var arr2 = mapper2.GetUserNames(2);
-            int a = mapper.UpdateUsers("MapperUpdated" + rand.Next(1, 100), 2);
-            var arr = mapper.GetUserNames(2);
+            var originMames = mapper.GetUserNames(2);
+            int count = mapper.UpdateUsers("MapperUpdatedName", 2);
+            var names = mapper.GetUserNames(2);
+            Assert.Equal(count, names.Length);
+            Assert.NotEqual(originMames[0], names[0]);
         }
     }
 
 
-    public interface ISpUserMapper : ISqlOperationMapper
-    {
-        [QuerySql("select name from person where age>@age")]
-        string[] GetUserNames(int age);
-
-        [QuerySql("select name from person where age>@age")]
-        IEnumerable<string> GetUserNames2(int age);
-
-        [ExcuteSql("update person set address=@address where age>@age")]
-        int UpdateUsers(string address, int age);
-    }
-
-    //public class SpUserMapper2 : ISpUserMapper
-    //{
-    //    private IDbConnection connection;
-
-    //    public SpUserMapper2(IDbConnection connection) => this.connection = connection;
-
-    //    public string[] GetUserNames(int age)
-    //    {
-    //        var sql = "select * from person where age>@age";
-    //        var dic = new Dictionary<string, dynamic>();
-    //        dic.Add("age", age);
-    //        return connection.Query<string>(sql, dic).ToArray();
-    //    }
-
-    //    public IEnumerable<string> GetUserNames2(int age)
-    //    {
-    //        var sql = "select * from person where age>@age";
-    //        var dic = new Dictionary<string, dynamic>();
-    //        dic.Add("age", age);
-    //        return connection.Query<string>(sql, dic);
-    //    }
-
-    //    public int UpdateUsers(string address, int age)
-    //    {
-    //        var sql = "111";
-    //        var dic = new Dictionary<string, dynamic>();
-    //        dic.Add("address", address);
-    //        dic.Add("age", age);
-    //        return connection.Execute(sql, dic);
-    //    }
-    //}
 }
