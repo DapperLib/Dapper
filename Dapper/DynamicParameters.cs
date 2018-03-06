05afae438b7bad68015967ddff45748afdf5c68a
Nathan Roe <nroe@zotecpartners.com>
2018-03-06 15:42:13 -0500

    Added back non-generic methods to fix issue with passing null value of type object

--- Dapper/DynamicParameters.cs
+++ Dapper/DynamicParameters.cs
@@ -100,6 +100,27 @@ namespace Dapper
         /// <param name="dbType">The type of the parameter.</param>
         /// <param name="direction">The in or out direction of the parameter.</param>
         /// <param name="size">The size of the parameter.</param>
+        public void Add(string name, object value, DbType? dbType, ParameterDirection? direction, int? size)
+        {
+            parameters[Clean(name)] = new ParamInfo
+            {
+                Name = name,
+                Value = value,
+                Type = value?.GetType(),
+                ParameterDirection = direction ?? ParameterDirection.Input,
+                DbType = dbType,
+                Size = size
+            };
+        }
+
+        /// <summary>
+        /// Add a parameter to this dynamic parameter list.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="dbType">The type of the parameter.</param>
+        /// <param name="direction">The in or out direction of the parameter.</param>
+        /// <param name="size">The size of the parameter.</param>
         public void Add<T>(string name, T value, DbType? dbType, ParameterDirection? direction, int? size)
         {
             parameters[Clean(name)] = new ParamInfo
@@ -110,6 +131,31 @@ namespace Dapper
                 ParameterDirection = direction ?? ParameterDirection.Input,
                 DbType = dbType,
                 Size = size
+            };
+        }
+
+        /// <summary>
+        /// Add a parameter to this dynamic parameter list.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="dbType">The type of the parameter.</param>
+        /// <param name="direction">The in or out direction of the parameter.</param>
+        /// <param name="size">The size of the parameter.</param>
+        /// <param name="precision">The precision of the parameter.</param>
+        /// <param name="scale">The scale of the parameter.</param>
+        public void Add(string name, object value = null, DbType? dbType = null, ParameterDirection? direction = null, int? size = null, byte? precision = null, byte? scale = null)
+        {
+            parameters[Clean(name)] = new ParamInfo
+            {
+                Name = name,
+                Value = value,
+                Type = value?.GetType(),
+                ParameterDirection = direction ?? ParameterDirection.Input,
+                DbType = dbType,
+                Size = size,
+                Precision = precision,
+                Scale = scale
             };
         }
 
