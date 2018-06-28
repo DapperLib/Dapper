b9b0bd2874ae91ed4a226ecde1a1c1bf94869736
Maksim Golev <Maksim_Golev@epam.com>
2018-06-28 12:23:15 +0400

    #259: Order of processing value was changed.

---
+++ Dapper.Tests/TestCases/EnumTestCases.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Dapper.Tests.TestEntities;
+
+namespace Dapper.Tests.TestCases
+{
+    public class EnumTestCases
+    {
+        public static IEnumerable<object[]> TestEnumParsingWithHandlerCases
+        {
+            get
+            {
+                yield return new object[] { "SELECT 1 AS Id, 'F' AS Type", new List<Item>() { new Item(1, ItemType.Foo) } };
+                yield return new object[] { "SELECT 22 AS Id, 'B' AS Type", new List<Item>() { new Item(22, ItemType.Bar) } };
+            }
+        }
+    }
+}
