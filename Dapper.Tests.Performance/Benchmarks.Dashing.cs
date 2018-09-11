b262cb33dd50102ae5e58f9fc487558771f77212
markjerz <mark@jerzykowski.me.uk>
2018-09-11 22:44:34 +0100

    Added Dashing Benchmarks (#1126)
    Adds Dashing benchmarks to Dapper.Tests.Performance

---
+++ Dapper.Tests.Performance/Benchmarks.Dashing.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using Dapper.Tests.Performance.Dashing;
+using Dashing;
+
+namespace Dapper.Tests.Performance
+{
+    public class DashingBenchmarks : BenchmarkBase
+    {
+        private ISession session;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            BaseSetup();
+            var configuration = new DashingConfiguration();
+            var database = new SqlDatabase(configuration, ConnectionString);
+            this.session = database.BeginTransactionLessSession(_connection);
+        }
+
+        [Benchmark(Description = "Get By Id")]
+        public Dashing.Post QueryBuffered()
+        {
+            Step();
+            return session.Get<Dashing.Post>(i);
+        }
+    }
+}
