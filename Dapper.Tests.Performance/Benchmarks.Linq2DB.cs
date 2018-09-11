b3a317c8cb3999201b545f147b1ed944a96af660
Nick Craver <craver@stackoverflow.com>
2018-09-11 17:41:58 -0400

    Benchmarks: Linq2DB tidy

--- Dapper.Tests.Performance/Benchmarks.Linq2DB.cs
+++ Dapper.Tests.Performance/Benchmarks.Linq2DB.cs
@@ -8,12 +8,12 @@ using LinqToDB.Data;
 
 namespace Dapper.Tests.Performance
 {
-    public class Liq2DbBenchmarks : BenchmarkBase
+    public class Linq2DBBenchmarks : BenchmarkBase
     {
         private Linq2DBContext _dbContext;
-        
-        private static Func<Linq2DBContext, int, Post> compiledQuery = CompiledQuery.Compile((Linq2DBContext db, int id) =>
-            db.Posts.First(c => c.Id == id));
+
+        private static readonly Func<Linq2DBContext, int, Post> compiledQuery = CompiledQuery.Compile((Linq2DBContext db, int id) => db.Posts.First(c => c.Id == id));
+
         [GlobalSetup]
         public void Setup()
         {
