9313e9f1f897d764645e4a2e31f3e2c34f1f9282
ifle <1140474+ifle@users.noreply.github.com>
2018-09-12 00:39:12 +0300

    Added Linq2DB benchmarks (#1125)
    Added Linq2DB benchmarks

---
+++ Dapper.Tests.Performance/Benchmarks.Linq2DB.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using System;
+using System.Linq;
+using Dapper.Tests.Performance.Linq2Db;
+using LinqToDB;
+using LinqToDB.Data;
+
+namespace Dapper.Tests.Performance
+{
+    public class Liq2DbBenchmarks : BenchmarkBase
+    {
+        private Linq2DBContext _dbContext;
+        
+        private static Func<Linq2DBContext, int, Post> compiledQuery = CompiledQuery.Compile((Linq2DBContext db, int id) =>
+            db.Posts.First(c => c.Id == id));
+        [GlobalSetup]
+        public void Setup()
+        {
+            BaseSetup();
+            DataConnection.DefaultSettings = new Linq2DBSettings(_connection.ConnectionString);
+            _dbContext = new Linq2DBContext();
+        }
+
+        [Benchmark(Description = "Normal")]
+        public Post Normal()
+        {
+            Step();
+            return _dbContext.Posts.First(p => p.Id == i);
+        }
+
+        [Benchmark(Description = "Compiled")]
+        public Post Compiled()
+        {
+            Step();
+            return compiledQuery(_dbContext, i);
+        }
+
+        [Benchmark(Description = "SqlQuery")]
+        public Post SqlQuery()
+        {
+            Step();
+            return _dbContext.Query<Post>("select * from Posts where Id = @id", new { id = i }).First();
+        }
+    }
+}
---
+++ Dapper.Tests.Performance/Linq2DB/ConnectionStringSettings.cs
@@ -0,0 +1,12 @@
+﻿using LinqToDB.Configuration;
+
+namespace Dapper.Tests.Performance.Linq2Db
+{
+    public class ConnectionStringSettings : IConnectionStringSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Name { get; set; }
+        public string ProviderName { get; set; }
+        public bool IsGlobal => false;
+    }
+}
