4f46cf0b5f0541d0e8b61195851070a118804b66
ifle <igor609@gmail.com>
2018-09-11 17:37:01 +0300

    Added Linq2DB benchmarks

---
+++ Dapper.Tests.Performance/Linq2DB/Linq2DbSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LinqToDB.Configuration;
+
+namespace Dapper.Tests.Performance.Linq2Db
+{
+    public class Linq2DBSettings : ILinqToDBSettings
+    {
+        private readonly string _connectionString;
+        public IEnumerable<IDataProviderSettings> DataProviders => Enumerable.Empty<IDataProviderSettings>();
+
+        public string DefaultConfiguration => "SqlServer";
+        public string DefaultDataProvider => "SqlServer";
+
+        public Linq2DBSettings(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<IConnectionStringSettings> ConnectionStrings
+        {
+            get
+            {
+                yield return
+                    new ConnectionStringSettings
+                    {
+                        Name = "SqlServer",
+                        ProviderName = "SqlServer",
+                        ConnectionString = _connectionString
+                    };
+            }
+        }
+    }
+}
