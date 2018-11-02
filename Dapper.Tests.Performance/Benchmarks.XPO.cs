82a005953573b00b1d4c39e43568fb45449dd8e4
Maksim Karpenko <38804406+maksimkarpenko@users.noreply.github.com>
2018-11-02 03:58:49 +0300

    Add the eXpress Persistent Objects (XPO) benchmark (#1131)

---
+++ Dapper.Tests.Performance/Benchmarks.XPO.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using System;
+using System.Linq;
+using System.ComponentModel;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+
+namespace Dapper.Tests.Performance
+{
+    [Description("DevExpress.XPO")]
+    public class XpoBenchmarks : BenchmarkBase
+    {
+        public UnitOfWork _session;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            BaseSetup();
+            IDataLayer dataLayer = XpoDefault.GetDataLayer(_connection, DevExpress.Xpo.DB.AutoCreateOption.SchemaAlreadyExists);
+            dataLayer.Dictionary.GetDataStoreSchema(typeof(Xpo.Post));
+            _session = new UnitOfWork(dataLayer, dataLayer);
+            _session.IdentityMapBehavior = IdentityMapBehavior.Strong;
+            _session.TypesManager.EnsureIsTypedObjectValid();
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _session.Dispose();
+        }
+
+        [Benchmark(Description = "GetObjectByKey<T>")]
+        public Xpo.Post GetObjectByKey()
+        {
+            Step();
+            return _session.GetObjectByKey<Xpo.Post>(i, true);
+        }
+
+        [Benchmark(Description = "FindObject<T>")]
+        public Xpo.Post FindObject()
+        {
+            Step();
+            CriteriaOperator _findCriteria = new BinaryOperator()
+            {
+                OperatorType = BinaryOperatorType.Equal,
+                LeftOperand = new OperandProperty("Id"),
+                RightOperand = new ConstantValue(i)
+            };
+            return _session.FindObject<Xpo.Post>(_findCriteria);
+        }
+
+        [Benchmark(Description = "Query<T>")]
+        public Xpo.Post Query()
+        {
+            Step();
+            return _session.Query<Xpo.Post>().First(p => p.Id == i);
+        }
+    }
+}
---
+++ Dapper.Tests.Performance/XPO/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DevExpress.Xpo;
+
+namespace Dapper.Tests.Performance.Xpo
+{
+    [Persistent("Posts")]
+    public class Post : XPLiteObject
+    {
+        [Key(false)]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastChangeDate { get; set; }
+        public int? Counter1 { get; set; }
+        public int? Counter2 { get; set; }
+        public int? Counter3 { get; set; }
+        public int? Counter4 { get; set; }
+        public int? Counter5 { get; set; }
+        public int? Counter6 { get; set; }
+        public int? Counter7 { get; set; }
+        public int? Counter8 { get; set; }
+        public int? Counter9 { get; set; }
+        public Post(Session session) : base(session) { }
+    }
+}
