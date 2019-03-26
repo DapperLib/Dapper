afa65d2b4428cc8f3ade62f7f6a01a856cc163c1
Marc Gravell <marc.gravell@gmail.com>
2019-03-26 10:34:00 +0000

    support the descriptor (component-model) API

---
+++ UIBindingTest/BindingForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+using Dapper;
+
+namespace UIBindingTest
+{
+    public partial class BindingForm : Form
+    {
+        public BindingForm()
+        {
+            InitializeComponent();
+
+            using (var conn = new SqlConnection("Data Source=.;Initial Catalog=master;Integrated Security=SSPI"))
+            {
+                mainGrid.DataSource = conn.Query("select * from sys.objects").AsList();
+            }
+        }
+    }
+}
