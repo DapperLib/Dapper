d8c4ce8e97db44bf42bff3736987cbf71b7df41d
mgravell <marc.gravell@gmail.com>
2019-08-30 13:33:04 +0100

    add Dapper.ProviderTools lib

---
+++ Dapper.Tests/ProviderTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.Common;
+using Dapper.ProviderTools;
+using Xunit;
+
+namespace Dapper.Tests
+{
+    public class ProviderTests
+    {
+        [Fact]
+        public void BulkCopy_SystemDataSqlClient()
+        {
+            using (var conn = new System.Data.SqlClient.SqlConnection())
+            {
+                Test<System.Data.SqlClient.SqlBulkCopy>(conn);
+            }
+        }
+
+        [Fact]
+        public void BulkCopy_MicrosoftDataSqlClient()
+        {
+            using (var conn = new Microsoft.Data.SqlClient.SqlConnection())
+            {
+                Test<Microsoft.Data.SqlClient.SqlBulkCopy>(conn);
+            }
+        }
+
+        private static void Test<T>(DbConnection connection)
+        {
+            using (var bcp = BulkCopy.TryCreate(connection))
+            {
+                Assert.NotNull(bcp);
+                Assert.IsType<T>(bcp.Wrapped);
+                bcp.EnableStreaming = true;
+            }
+        }
+
+        [Theory]
+        [InlineData(51000, 51000, true)]
+        [InlineData(51000, 43, false)]
+        public void DbNumber_SystemData(int create, int test, bool result)
+            => Test<SystemSqlClientProvider>(create, test, result);
+
+        [Theory]
+        [InlineData(51000, 51000, true)]
+        [InlineData(51000, 43, false)]
+        public void DbNumber_MicrosoftData(int create, int test, bool result)
+            => Test<MicrosoftSqlClientProvider>(create, test, result);
+
+        private void Test<T>(int create, int test, bool result)
+            where T : SqlServerDatabaseProvider, new()
+        {
+            var provider = new T();
+            using (var conn = provider.GetOpenConnection())
+            {
+                try
+                {
+                    conn.Execute("throw @create, 'boom', 1;", new { create });
+                    Assert.False(true);
+                }
+                catch(DbException err)
+                {
+                    Assert.Equal(result, err.IsNumber(test));
+                }
+            }
+        }
+    }
+}
