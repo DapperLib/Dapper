33b9bc0ba197117a09df69e237b8ee057f3dcfdd
Nick Craver <craver@stackoverflow.com>
2019-09-22 10:27:58 -0400

    Linq2DB Benchmarks: level the playing field
    I dug into why Linq2DB was so much faster...it's because it wasn't mapping the results out. When we actually add the columns and do the same work, times increase darmtically (20-60% depending on op)

--- Dapper.Tests.Performance/Post.cs
+++ Dapper.Tests.Performance/Post.cs
@@ -11,17 +11,29 @@ namespace Dapper.Tests.Performance
         [Id(IdKind.Identity)]
         [LinqToDB.Mapping.PrimaryKey, LinqToDB.Mapping.Identity]
         public int Id { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public string Text { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.NotNull]
         public DateTime CreationDate { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.NotNull]
         public DateTime LastChangeDate { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter1 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter2 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter3 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter4 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter5 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter6 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter7 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter8 { get; set; }
+        [LinqToDB.Mapping.Column, LinqToDB.Mapping.Nullable]
         public int? Counter9 { get; set; }
     }
 }
