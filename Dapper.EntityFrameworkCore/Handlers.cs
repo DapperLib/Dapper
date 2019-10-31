71ebca991f6e6b7df8fccddd193a7450a3cd9d25
Money <abc12207@gmail.com>
2019-11-01 00:25:12 +0800

    add Dapper.EntityFrameworkCore for NetTopologySuite handler

---
+++ Dapper.EntityFrameworkCore/Handlers.cs
@@ -0,0 +1,40 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace Dapper.EntityFrameworkCore
+{
+    /// <summary>
+    /// Acts on behalf of all type-handlers in this package
+    /// </summary>
+    public static class Handlers
+    {
+        /// <summary>
+        /// Register geography type-handlers in this package
+        /// </summary>
+        public static void RegisterGeography()
+        {
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<Point>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<Polygon>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<LineString>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<LinearRing>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<MultiLineString>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<MultiPoint>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<MultiPolygon>(true));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<Geometry>(true));
+        }
+
+        /// <summary>
+        /// Register geometry type-handlers in this package
+        /// </summary>
+        public static void RegisterGeometry()
+        {
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<Point>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<Polygon>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<LineString>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<LinearRing>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<MultiLineString>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<MultiPoint>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<MultiPolygon>(false));
+            SqlMapper.AddTypeHandler(new NetTopologySuiteGeometryHandler<Geometry>(false));
+        }
+    }
+}
---
+++ Dapper.EntityFrameworkCore/NetTopologySuiteGeometryHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace Dapper.EntityFrameworkCore
+{
+    /// <summary>
+    /// Type-handler for the Geometry spatial type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class NetTopologySuiteGeometryHandler<T> : SqlMapper.TypeHandler<T>
+        where T : Geometry
+    {
+        readonly bool _geography;
+        readonly SqlServerBytesWriter _writer;
+        readonly SqlServerBytesReader _reader;
+
+        /// <summary>
+        /// Create a new handler instance.
+        /// </summary>
+        /// <param name="geography"></param>
+        public NetTopologySuiteGeometryHandler(bool geography = false)
+        {
+            _geography = geography;
+            _writer = new SqlServerBytesWriter { IsGeography = geography };
+            _reader = new SqlServerBytesReader { IsGeography = geography };
+        }
+
+        /// <summary>
+        /// Parse a database value back to a typed value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override T Parse(object value)
+            => (T)_reader.Read((byte[])value);
+
+        /// <summary>
+        /// Assign the value of a parameter before a command executes.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        public override void SetValue(IDbDataParameter parameter, T value)
+        {
+            parameter.Value = _writer.Write(value);
+
+            ((SqlParameter)parameter).SqlDbType = SqlDbType.Udt;
+            ((SqlParameter)parameter).UdtTypeName = _geography ? "geography" : "geometry";
+        }
+    }
+
+}
