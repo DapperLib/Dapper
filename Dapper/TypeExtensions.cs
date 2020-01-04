6a5517d7afc8aaafadd4c68fe9277206d799be90
jnm2 <me@jnm2.com>
2020-01-04 16:00:41 -0500

    Enable NRTs in TypeExtensions.cs

--- Dapper/TypeExtensions.cs
+++ Dapper/TypeExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Reflection;
 
+#nullable enable
+
 namespace Dapper
 {
     internal static class TypeExtensions
     {
-        public static MethodInfo GetPublicInstanceMethod(this Type type, string name, Type[] types)
+        public static MethodInfo? GetPublicInstanceMethod(this Type type, string name, Type[] types)
             => type.GetMethod(name, BindingFlags.Instance | BindingFlags.Public, null, types, null);
     }
 }
