a7b80018271007030edc6f287dd932b7f3d66aac
ryan-todd <42576404+ryan-todd@users.noreply.github.com>
2020-01-29 08:23:07 +0000

    Use `AppendColumnName` with Contrib's `Insert`

--- Dapper.Contrib/SqlMapperExtensions.cs
+++ Dapper.Contrib/SqlMapperExtensions.cs
@@ -1007,7 +1007,7 @@ public partial class PostgresAdapter : ISqlAdapter
                 if (!first)
                     sb.Append(", ");
                 first = false;
-                sb.Append($"\"{property.Name}\"");
+                AppendColumnName(sb, property.Name);
             }
         }
 
