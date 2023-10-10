a37b151bb745ed0763441aa50f29c504221aa7d3
Marc Gravell <marc.gravell@gmail.com>
2023-10-10 15:06:21 +0100

    auto-detect stored procedures as anything without whitespace (#1975)
    * 1. auto-detect stored procedures as anything without whitespace
    2. we have public fields? (hangs head in shame)
    
    * release notes
    
    * no need to use regex

--- Dapper/CommandDefinition.cs
+++ Dapper/CommandDefinition.cs
@@ -48,10 +48,15 @@ namespace Dapper
         /// </summary>
         public int? CommandTimeout { get; }
 
+        internal readonly CommandType CommandTypeDirect;
+
         /// <summary>
         /// The type of command that the command-text represents
         /// </summary>
-        public CommandType? CommandType { get; }
+#if DEBUG // prevent use in our own code
+        [Obsolete("Prefer " + nameof(CommandTypeDirect), true)]
+#endif
+        public CommandType? CommandType => CommandTypeDirect;
 
         /// <summary>
         /// Should data be buffered before returning?
@@ -92,10 +97,20 @@ namespace Dapper
             Parameters = parameters;
             Transaction = transaction;
             CommandTimeout = commandTimeout;
-            CommandType = commandType;
+            CommandTypeDirect = commandType ?? InferCommandType(commandText);
             Flags = flags;
             CancellationToken = cancellationToken;
+
+            static CommandType InferCommandType(string sql)
+            {
+                if (sql is null || sql.IndexOfAny(WhitespaceChars) >= 0) return System.Data.CommandType.Text;
+                return System.Data.CommandType.StoredProcedure;
+            }
         }
+
+        // if the sql contains any whitespace character (space/tab/cr/lf): interpret as ad-hoc; but "SomeName" should be treated as a stored-proc
+        // (note TableDirect would need to be specified explicitly, but in reality providers don't usually support TableDirect anyway)
+        private static readonly char[] WhitespaceChars = new char[] { ' ', '\t', '\r', '\n' };
 
         private CommandDefinition(object? parameters) : this()
         {
@@ -124,8 +139,7 @@ namespace Dapper
             {
                 cmd.CommandTimeout = SqlMapper.Settings.CommandTimeout.Value;
             }
-            if (CommandType.HasValue)
-                cmd.CommandType = CommandType.Value;
+            cmd.CommandType = CommandTypeDirect;
             paramReader?.Invoke(cmd, Parameters);
             return cmd;
         }
