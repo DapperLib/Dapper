3da35dc8b394514f18984fbbcbe50df241ec942b
Dmitrii Korolev <dmkorolev@microsoft.com>
2023-10-14 22:36:03 +0200

    add method target for explicitconstructor

--- Dapper/ExplicitConstructorAttribute.cs
+++ Dapper/ExplicitConstructorAttribute.cs
@@ -5,7 +5,7 @@ namespace Dapper
     /// <summary>
     /// Tell Dapper to use an explicit constructor, passing nulls or 0s for all parameters
     /// </summary>
-    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class ExplicitConstructorAttribute : Attribute
     {
     }
