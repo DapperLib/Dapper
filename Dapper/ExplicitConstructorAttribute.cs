c1cc104d41f35c8e8efd1034892e036fcd062ad1
Dmitrii Korolev <dmkorolev@microsoft.com>
2023-10-15 01:44:35 +0200

    add a noting comment (usage is limited with Dapper.AOT)

--- Dapper/ExplicitConstructorAttribute.cs
+++ Dapper/ExplicitConstructorAttribute.cs
@@ -5,6 +5,9 @@ namespace Dapper
     /// <summary>
     /// Tell Dapper to use an explicit constructor, passing nulls or 0s for all parameters
     /// </summary>
+    /// <remarks>
+    /// Usage on methods is limited to the usage with Dapper.AOT (https://github.com/DapperLib/DapperAOT)
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class ExplicitConstructorAttribute : Attribute
     {
