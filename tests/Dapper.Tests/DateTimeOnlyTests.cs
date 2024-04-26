5ccac9736178c0afec58b30f61ed94b09448af79
Marc Gravell <marc.gravell@gmail.com>
2024-04-26 08:49:47 +0100

    disable DateOnly / TimeOnly support - see #2071 / #2072

--- tests/Dapper.Tests/DateTimeOnlyTests.cs
+++ tests/Dapper.Tests/DateTimeOnlyTests.cs
@@ -9,7 +9,7 @@ namespace Dapper.Tests;
 [Collection("DateTimeOnlyTests")]
 public sealed class SystemSqlClientDateTimeOnlyTests : DateTimeOnlyTests<SystemSqlClientProvider> { }
 */
-#if MSSQLCLIENT
+#if MSSQLCLIENT && DATEONLY
 [Collection("DateTimeOnlyTests")]
 public sealed class MicrosoftSqlClientDateTimeOnlyTests : DateTimeOnlyTests<MicrosoftSqlClientProvider> { }
 #endif
@@ -17,8 +17,11 @@ public abstract class DateTimeOnlyTests<TProvider> : TestBase<TProvider> where T
 {
     public class HazDateTimeOnly
     {
+        public string Name { get; set; }
         public DateOnly Date { get; set; }
         public TimeOnly Time { get; set; }
+        public DateOnly? NDate { get; set; }
+        public TimeOnly? NTime { get; set; }
     }
 
     [Fact]
@@ -27,12 +30,18 @@ public abstract class DateTimeOnlyTests<TProvider> : TestBase<TProvider> where T
         var now = DateTime.Now;
         var args = new HazDateTimeOnly
         {
+            Name = nameof(TypedInOut),
             Date = DateOnly.FromDateTime(now),
             Time = TimeOnly.FromDateTime(now),
+            NDate = DateOnly.FromDateTime(now),
+            NTime = TimeOnly.FromDateTime(now),
         };
-        var row = connection.QuerySingle<HazDateTimeOnly>("select @date as [Date], @time as [Time]", args);
+        var row = connection.QuerySingle<HazDateTimeOnly>("select @name as [Name], @date as [Date], @time as [Time], @ndate as [NDate], @ntime as [NTime]", args);
+        Assert.Equal(args.Name, row.Name);
         Assert.Equal(args.Date, row.Date);
         Assert.Equal(args.Time, row.Time);
+        Assert.Equal(args.NDate, row.NDate);
+        Assert.Equal(args.NTime, row.NTime);
     }
 
     [Fact]
@@ -41,12 +50,18 @@ public abstract class DateTimeOnlyTests<TProvider> : TestBase<TProvider> where T
         var now = DateTime.Now;
         var args = new HazDateTimeOnly
         {
+            Name = nameof(TypedInOutAsync),
             Date = DateOnly.FromDateTime(now),
             Time = TimeOnly.FromDateTime(now),
+            NDate = DateOnly.FromDateTime(now),
+            NTime = TimeOnly.FromDateTime(now),
         };
-        var row = await connection.QuerySingleAsync<HazDateTimeOnly>("select @date as [Date], @time as [Time]", args);
+        var row = await connection.QuerySingleAsync<HazDateTimeOnly>("select @name as [Name], @date as [Date], @time as [Time], @ndate as [NDate], @ntime as [NTime]", args);
+        Assert.Equal(args.Name, row.Name);
         Assert.Equal(args.Date, row.Date);
         Assert.Equal(args.Time, row.Time);
+        Assert.Equal(args.NDate, row.NDate);
+        Assert.Equal(args.NTime, row.NTime);
     }
 
     [Fact]
@@ -54,11 +69,14 @@ public abstract class DateTimeOnlyTests<TProvider> : TestBase<TProvider> where T
     {
         var now = DateTime.Now;
         var args = new DynamicParameters();
+        var name = nameof(UntypedInOut);
         var date = DateOnly.FromDateTime(now);
         var time = TimeOnly.FromDateTime(now);
+        args.Add("name", name);
         args.Add("date", date);
         args.Add("time", time);
-        var row = connection.QuerySingle<dynamic>("select @date as [Date], @time as [Time]", args);
+        var row = connection.QuerySingle<dynamic>("select @name as [Name], @date as [Date], @time as [Time]", args);
+        Assert.Equal(name, (string)row.Name);
         // untyped, observation is that these come back as DateTime and TimeSpan
         Assert.Equal(date, DateOnly.FromDateTime((DateTime)row.Date));
         Assert.Equal(time, TimeOnly.FromTimeSpan((TimeSpan)row.Time));
